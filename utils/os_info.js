69ebbc4d1c2c5e8b5021c2b3ae154a625256480f
seapp88 <seapp88@icloud.com>
2019-08-17 20:16:37 +0300

    Edit network

--- utils/os_info.js
+++ utils/os_info.js
@@ -1,44 +1,70 @@
 let os_utils = require('os-utils');
-
+let pi = require('node-raspi');
 let os = require('os');
-let ifaces = os.networkInterfaces();
 
-let ip = '';
+
+
 let start_time = new Date();
 
-Object.keys(ifaces).forEach(function (ifname) {
-    var alias = 0;
+let getIP = () => {
+    let ifaces = os.networkInterfaces();
+    let ip = '';
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
 
-    ifaces[ifname].forEach(function (iface) {
-        if ('IPv4' !== iface.family || iface.internal !== false) {
-            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-            return;
-        }
-
-        if (alias >= 1) {
-            // this single interface has multiple ipv4 addresses
-            console.log(ifname + ':' + alias, iface.address);
-        } else {
-            // this interface has only one ipv4 adress
-            console.log(ifname, iface.address);
-
-            if(ifname === 'eth0'){
-                ip = iface.address
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
             }
-        }
-        ++alias;
-    });
-});
 
-setInterval(() => {
-    os_utils.cpuUsage((v) => {
-        let res = {
-            cpu: (v * 100).toFixed(),
-            ram: ((os_utils.totalmem() - os_utils.freemem()) / os_utils.totalmem() * 100).toFixed(),
-            ip,
-            start_time
-        }
-        io.sockets.emit('info:os', res);
-        // console.log( 'CPU Usage (%): ' + v );
+            if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                console.log(ifname + ':' + alias, iface.address);
+            } else {
+                // this interface has only one ipv4 adress
+                console.log(ifname, iface.address);
+
+                if(ifname === 'eth0'){
+                    ip = iface.address
+                }
+            }
+            ++alias;
+        });
     });
-}, 5000);
+
+    return ip;
+};
+
+let getInfo = () => {
+    return new Promise((resolve, reject) => {
+        os_utils.cpuUsage((v) => {
+            res = {
+                cpu: (v * 100).toFixed(),
+                ram: ((os_utils.totalmem() - os_utils.freemem()) / os_utils.totalmem() * 100).toFixed(),
+                ip: getIP(),
+                start_time,
+                temp: pi.getThrm(),
+                volt: pi.getVcc(),
+                uptime_os: pi.uptime()
+            };
+            resolve(res);
+        });
+    })
+};
+
+let run = async () => {
+    try {
+        let res = await getInfo();
+        console.log(res);
+        io.sockets.emit('info:os', res);
+
+        setTimeout(() => {
+            run();
+        }, 5000)
+    }catch (e) {
+        console.log(e)
+    }
+};
+
+run();
